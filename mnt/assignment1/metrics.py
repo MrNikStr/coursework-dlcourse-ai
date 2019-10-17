7994c159d0ed3c1e7e054b1682a9549738421e32
Alexander Demidko <alexander@fluence.one>
2019-10-17 14:59:11 +0300

    Exercise 4 - completed

--- mnt/assignment1/metrics.py
+++ mnt/assignment1/metrics.py
@@ -2,40 +2,41 @@ import numpy as np
 
 
 def binary_classification_metrics(prediction, ground_truth):
-    '''
+    """
     Computes metrics for binary classification
 
     Arguments:
-    prediction, np array of bool (num_samples) - model predictions
-    ground_truth, np array of bool (num_samples) - true labels
+        prediction, np array of int (num_samples) - model predictions
+        ground_truth, np array of int (num_samples) - true labels
 
     Returns:
-    precision, recall, f1, accuracy - classification metrics
-    '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
+        precision, recall, f1, accuracy - classification metrics
+    """
 
-    # TODO: implement metrics!
-    # Some helpful links:
-    # https://en.wikipedia.org/wiki/Precision_and_recall
-    # https://en.wikipedia.org/wiki/F1_score
-    
+    tp = float(np.sum((prediction == 1) & (ground_truth == 1)))
+    tn = float(np.sum((prediction == 0) & (ground_truth == 0)))
+    fp = float(np.sum((prediction == 1) & (ground_truth == 0)))
+    fn = float(np.sum((prediction == 0) & (ground_truth == 1)))
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    f1 = 2 * precision * recall / (precision + recall)
+
     return precision, recall, f1, accuracy
 
 
 def multiclass_accuracy(prediction, ground_truth):
-    '''
+    """
     Computes metrics for multiclass classification
 
     Arguments:
-    prediction, np array of int (num_samples) - model predictions
-    ground_truth, np array of int (num_samples) - true labels
+        prediction, np array of int (num_samples) - model predictions
+        ground_truth, np array of int (num_samples) - true labels
 
     Returns:
-    accuracy - ratio of accurate predictions to total samples
-    '''
+        accuracy - ratio of accurate predictions to total samples
+    """
 
     assert prediction.shape == ground_truth.shape
     assert prediction.ndim == 1
